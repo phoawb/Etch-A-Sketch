13aa8fde4b1b7d8ab7ed88c2229f806135ecb199
Philip Welin-Berger <philip.welin.berger@gmail.com>
2022-09-05 21:35:36 +0200

    added logic to be able to toggle the active button

--- script.js
+++ script.js
@@ -9,8 +9,9 @@ because Safari renders gradients different for some reasons*/
   }
 })();
 
-const grid = document.querySelector('.grid');
-const slider = document.getElementById('slider');
+const GRID = document.querySelector('.grid');
+const SLIDER = document.getElementById('slider');
+const BUTTONS = [...document.getElementsByClassName('button')];
 let sliderValue = document.getElementById('sliderValue');
 let gridSize = 16;
 
@@ -18,13 +19,13 @@ const createGrid = (gridSquared) => {
   for (let i = 0; i < gridSquared; i++) {
     const cell = document.createElement('div');
     cell.classList = 'cell';
-    grid.appendChild(cell);
+    GRID.appendChild(cell);
   }
 };
 
 const deleteGrid = (gridSquared) => {
   for (let i = 0; i < gridSquared; i++) {
-    grid.removeChild(grid.firstChild);
+    GRID.removeChild(GRID.firstChild);
   }
 };
 
@@ -37,15 +38,36 @@ const adjustGrid = (gridSizeDiff) => {
 };
 
 //Update the slider value label in real time
-slider.addEventListener('input', (e) => {
+SLIDER.addEventListener('input', (e) => {
   const oldGridSize = gridSize;
   gridSize = Number(e.target.value);
   sliderValue.innerText = `${gridSize}`;
-  grid.setAttribute(
+  GRID.setAttribute(
     'style',
-    `grid-template-columns: repeat(${gridSize}, 2fr); grid-template-rows: repeat(${gridSize}, 2fr);`
+    `GRID-template-columns: repeat(${gridSize}, 2fr); GRID-template-rows: repeat(${gridSize}, 2fr);`
   );
   adjustGrid(gridSize * gridSize - oldGridSize * oldGridSize);
 });
 
-createGrid(gridSize * gridSize);
+const toggleButton = (button) => {
+  if (button.classList === 'active-button') return;
+  BUTTONS.forEach((b) => b.classList === 'button');
+  console.log('DO I TOGGLE THE BUTTON?');
+  button.classList = 'active-button';
+};
+
+const activateButtons = () => {
+  BUTTONS.forEach((b) =>
+    b.addEventListener('click', () => {
+      if (b.classList === 'active-button') return;
+      BUTTONS.forEach((b) => (b.classList = 'button'));
+      console.log('DO I TOGGLE THE BUTTON?');
+      b.classList = 'active-button';
+    })
+  );
+};
+
+window.onload = () => {
+  createGrid(gridSize * gridSize);
+  activateButtons();
+};
